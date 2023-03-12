8d6cb953c20650a2cd28d4a2a9a9112b63550998
Navansh <khandelwalnavansh1@gmail.com>
2023-03-12 13:47:29 +0530

    Commit 10 March_2

---
+++ IntelliJ/Recursion/src/Questions_and_Concepts_of_Recursions/Leetcode_1155_Dice_Rolls_With_Target_Sum.java
@@ -0,0 +1,4 @@
+package Questions_and_Concepts_of_Recursions;
+
+public class Leetcode_1155_Dice_Rolls_With_Target_Sum {
+}
--- IntelliJ/Recursion/src/Questions_and_Concepts_of_Recursions/Leetcode_17_Phone_pattern.java
+++ IntelliJ/Recursion/src/Questions_and_Concepts_of_Recursions/Leetcode_17_Phone_pattern.java
@@ -1,9 +1,15 @@
 package Questions_and_Concepts_of_Recursions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_17_Phone_pattern {
     public static void main(String[] args) {
-        Pad("","12");
+//        Pad("","12");
+        List<String> ans = PadReturnLeetcode("","23");
+        System.out.println(ans);
     }
+
     static void Pad(String processed, String unprocessed){
         if (unprocessed.isEmpty())
         {
@@ -19,5 +25,59 @@ public class Leetcode_17_Phone_pattern {
             Pad(processed+ch,unprocessed.substring(1));
         }
 
+    }
+
+    static ArrayList<String> PadReturn(String processed, String unprocessed){
+        if (unprocessed.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        int digit = unprocessed.charAt(0) -'0';
+        //this will convert '2' to 2
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = (digit - 1)*3 ; i < (digit*3); i++) {
+            char ch = (char)('a' + i);
+            ans.addAll(PadReturn(processed+ch,unprocessed.substring(1)));
+        }
+
+        return ans;
+    }
+    static List<String> PadReturnLeetcode(String processed, String unprocessed){
+        if (unprocessed.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        int digit = unprocessed.charAt(0) -'1';
+        //this will convert '2' to 2
+        ArrayList<String> ans = new ArrayList<>();
+        if (digit==6){
+            for (int i = (digit - 1)*3 ; i < (digit*3) + 1; i++) {
+                char ch = (char)('a' + i);
+                ans.addAll(PadReturnLeetcode(processed+ch,unprocessed.substring(1)));
+            }
+        } else if (digit == 7) {
+            for (int i = (digit - 1)*3 + 1 ; i < (digit*3) + 1; i++) {
+                char ch = (char)('a' + i);
+                ans.addAll(PadReturnLeetcode(processed+ch,unprocessed.substring(1)));
+            }
+        } else if (digit==8) {
+            for (int i = (digit - 1)*3 + 1 ; i < (digit*3) + 2; i++) {
+                char ch = (char)('a' + i);
+                ans.addAll(PadReturnLeetcode(processed+ch,unprocessed.substring(1)));
+            }
+        }
+        else
+            for (int i = (digit - 1)*3 ; i < (digit*3); i++) {
+                char ch = (char)('a' + i);
+                ans.addAll(PadReturnLeetcode(processed+ch,unprocessed.substring(1)));
+            }
+
+        return ans;
     }
 }
