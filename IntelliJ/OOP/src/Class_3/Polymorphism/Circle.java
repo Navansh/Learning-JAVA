4fa883bc704eb338de9d0569c069e704db8ddd7c
Navansh <khandelwalnavansh1@gmail.com>
2023-03-17 02:14:38 +0530

    DO

--- IntelliJ/OOP/src/Class_3/Polymorphism/Circle.java
+++ IntelliJ/OOP/src/Class_3/Polymorphism/Circle.java
@@ -1,4 +1,13 @@
 package Class_3.Polymorphism;
 
-public class Circle {
+public class Circle extends Shapes {
+    @Override //simply an annotation or a check that tells this method is being overriden by its sub-class(of Shapes) methods
+    public void area(){
+        System.out.println("This is area of Circle");
+    }
+//    @Override
+    static void doit(){
+        System.out.println("It has overriden the static method from the parent class ");
+    }
+
 }
--- IntelliJ/OOP/src/Class_3/Polymorphism/Shapes.java
+++ IntelliJ/OOP/src/Class_3/Polymorphism/Shapes.java
@@ -1,4 +1,10 @@
 package Class_3.Polymorphism;
 
 public class Shapes {
+    public void area(){
+        System.out.println("defwf");
+    }
+    static void doit(){
+        System.out.println("This is a static method which can be inherited but cannot be overriden");
+    }
 }
