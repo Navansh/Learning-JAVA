8d94f4bcf9cf11ae9b70f961bde62385ce6e22bc
Navansh <khandelwalnavansh1@gmail.com>
2023-03-17 14:17:33 +0530

    DO

--- IntelliJ/OOP/src/Class_3/Polymorphism/Main.java
+++ IntelliJ/OOP/src/Class_3/Polymorphism/Main.java
@@ -3,6 +3,6 @@ package Class_3.Polymorphism;
 public class Main {
     public static void main(String[] args) {
         Circle c1 = new Circle();
-        Circle.doit();
+        c1.doit();
     }
 }
