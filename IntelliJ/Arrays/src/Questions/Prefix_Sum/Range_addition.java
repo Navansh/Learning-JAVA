419b71102c093296e158456d89611a2e33dea3d5
Navansh <khandelwalnavansh1@gmail.com>
2024-02-07 21:01:59 +0530

    Do

---
+++ IntelliJ/Arrays/src/Questions/Prefix_Sum/Range_addition.java
@@ -0,0 +1,15 @@
+package Questions.Prefix_Sum;
+
+public class Range_addition {
+    public static void main(String[] args) {
+        //Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+        //Each operation is represented as a triplet: [startIndex, endIndex, inc]
+        // which increments each element of subarray A[startIndex ... endIndex]
+        // (startIndex and endIndex inclusive) with inc.
+
+        //Return the modified array after all k operations were executed.
+
+
+    }
+}
---
+++ IntelliJ/Heaps/src/Questions/Sort_Chars_by_freq.java
@@ -0,0 +1,59 @@
+package Questions;
+
+import java.util.*;
+
+public class Sort_Chars_by_freq {
+    public static void main(String[] args) {
+//https://leetcode.com/problems/sort-characters-by-frequency/
+        System.out.println(frequencySort("tree"));
+    }
+
+    public static String frequencySort(String s) {
+
+        //This is not a correct solution, check LC solution for more info****
+        //**********************************************************************
+
+
+
+
+
+        HashMap<Integer, Integer> hx = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            int temp = ch + '0';
+            if(hx.containsKey(temp)) {
+                int freq = hx.get(temp);
+                hx.put(temp, freq + 1);
+            } else {
+                hx.put(temp, 1);
+            }
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        //this is a max Priority Queue
+
+        //add all the elements of our Hashmap into this
+        StringBuilder str = new StringBuilder();
+        for (int val :
+                hx.keySet()) {
+            pq.add(hx.get(val));
+        }
+
+        //now for every freq, remove it get the corresponding element from it and add
+        for(int i = 0; i < pq.size(); i++) {
+            //get the key of the peeked element in the pq from hashmap
+            int peeked_ele = pq.peek();
+
+        }
+        for (Map.Entry<Integer, Integer> entry : hx.entrySet()) {
+//            if (Objects.equals(entry.getValue(), pq.peek())) {
+//                int foundKey = entry.getKey();
+//                //add this key to the stringbuilder as a chart
+//                str.append((char) foundKey);
+//            }
+            System.out.println(pq.peek());
+        }
+
+        return str.toString();
+    }
+}
