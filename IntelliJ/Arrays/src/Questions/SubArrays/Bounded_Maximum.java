179b5ae3e2e75114d24a650e04e97f5a0c091184
Navansh <khandelwalnavansh1@gmail.com>
2024-02-10 00:34:07 +0530

    Do

---
+++ IntelliJ/Arrays/src/Questions/SubArrays/Bounded_Maximum.java
@@ -0,0 +1,12 @@
+package Questions.SubArrays;
+
+public class Bounded_Maximum {
+    public static void main(String[] args) {
+//https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+
+    }
+
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+
+    }
+}
