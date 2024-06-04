2b9d645ec57d5bdb1b43cb258f688118984df2ad
Navansh <khandelwalnavansh1@gmail.com>
2024-06-04 23:27:59 +0530

    Do

--- IntelliJ/Trie/src/Questions/Find_URL.java
+++ IntelliJ/Trie/src/Questions/Find_URL.java
@@ -20,11 +20,39 @@ public class Find_URL {
     }
 
     //AMAZON BERLIN QUES
+    //DON'T KNOW IF THE CODE IS CORRECT ****************************
     public static boolean findURL(String[] arr, String query) {
         //given query check if the query is available in the string set given in arr
         //insert in Trie
         TrieNode root = new TrieNode();
         insertTrie(arr, root);
+        return searchInTrie(root, query, 0);
+    }
+
+    private static boolean searchInTrie(TrieNode root, String query, int k) {
+        String[] toFind = query.split("/");
+        TrieNode temp = root;
+        boolean usual = false;
+        boolean id = false;
+
+        for (int i = k; i < toFind.length; i++) {
+            String to = toFind[i];
+            if(temp.hx.containsKey(to)) {
+                //go to that string
+                usual = searchInTrie(temp.hx.get(to), query, i + 1);
+                //check  if we get the ans going through the usual path
+            } else if (temp.hx.containsKey("{id}")) {
+                //now we have to find recursively
+                id = searchInTrie(temp.hx.get("{id}"), query, i + 1);
+                //next string se hi start karoge as ye toh milgyi ya phir id hai
+            } else {
+                return false;
+            }
+        }
+
+        return usual || id;
+        //meaning if usual is true then return true else if id is true then return
+        //true else return false
     }
 
     private static void insertTrie(String[] arr, TrieNode root) {
@@ -36,9 +64,18 @@ public class Find_URL {
             //now split this
             String[] toInsert = get.split("/");
             //now insert each of this String into the Trie
-            if(temp.hx.containsKey())
-
+            for (int j = 0; j < toInsert.length; j++) {
+                String to = toInsert[i];
+                if(temp.hx.containsKey(to)) {
+                    //jump temp to that
+                    temp = temp.hx.get(to);
+                } else {
+                    TrieNode forch = new TrieNode();
+                    temp.hx.put(to, forch);
+                    temp = temp.hx.get(to);
+                }
+            }
+            temp.endOfWord = true;
         }
-
     }
 }
