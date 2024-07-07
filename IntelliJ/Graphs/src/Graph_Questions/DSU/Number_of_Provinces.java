2f9e7fd5e21800a5e9ba6e3dedeb582a3c2514cd
Navansh <khandelwalnavansh1@gmail.com>
2024-07-07 22:09:55 +0530

    Do

--- IntelliJ/Graphs/src/Graph_Questions/DSU/Number_of_Provinces.java
+++ IntelliJ/Graphs/src/Graph_Questions/DSU/Number_of_Provinces.java
@@ -1,5 +1,6 @@
 package Graph_Questions.DSU;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Number_of_Provinces {
@@ -14,17 +15,24 @@ public class Number_of_Provinces {
         //initially all the values are just the index values itself
         //signifying that each one of the node is parent of itself
 
+        int[] rank = new int[isConnected.length];
+        //this will store the length of chain of leaders
+
+        //initially every rank is 1
+        Arrays.fill(rank, 1);
+
+
         for (int i = 0; i < parent.length; i++) {
             parent[i] = i;
         }
 
-        for (int i = 0; i < isConnected.length; i++) {
+        for (int i = 0; i < isConnected.length; i++) { // O(n * n * 1)
             for (int j = 0; j < isConnected[i].length; j++) {
                 if(isConnected[i][j] == 1) {
                     int u = i;
                     int v = j;
 
-                    union(u,v, parent);
+                    union(u,v, parent, rank);
                 }
             }
         }
@@ -32,36 +40,71 @@ public class Number_of_Provinces {
         //now we have the parent array filled
         //so to find the number of disjoint sets
         //we need to check for the condition that
-        //a parent is that that points to itself
+        //a parent is that which points to itself
+        //hence : parent[i] == i
         int count = 0;
         for (int i = 0; i < parent.length; i++) {
+            //O(V)
             if(parent[i] == i) {
                 count++;
             }
         }
         return count;
+
+        //total TX : O(n*n) + O(V)
+        // == O(E + V ) where E represents the number of edges
+        //and V represents Vertices
     }
 
-    private void union(int x, int y, int[] parent) {
+    private void union(int x, int y, int[] parent, int[] rank) {
+        //as find function is O(1) on avg basis,
+        //so this function is O(2 * O(1) ) ~ O(1)
+
         int parentOfX = find(x, parent);
         int parentOfY = find(y, parent);
 
-        parent[parentOfX] = parentOfY;
-    }
-
-    private int find(int x, int[] parent) {
-        //to find the leader, the index and the value should
-        //match
-        if(x == parent[x]) {
-            return x;
+        if(parentOfX == parentOfY) {
+            //then we don't need to do anything
+            //as they're already in same set
+            return;
         }
 
-        int temp = find(parent[x], parent);
+        //here we'll now do Union by rank Optimization
+        if(rank[parentOfX] > rank[parentOfY]) {
+            //making the leader of longer chain as the new leader
+            //hence which has more rank
+            parent[parentOfY] = parentOfX;
+            //doing this the rank of the leader doesn't increase
+        } else if(rank[parentOfX] < rank[parentOfY]) {
+            parent[parentOfX] = parentOfY;
+            //doing this the rank of the leader doesn't increase
+        } else {
+            //can make any one of them as the new leader, say this
+            parent[parentOfX] = parentOfY;
+            //here the rank increases
+            //hence the rank of new leader increases by 1
+            rank[parentOfY]++;
+        }
+    }
+
+    private int find(int ele, int[] parent) {
+        //O(1) - Avg case TC : cz of the path compression optimization
+        //worst Case TC is still O(n) where we find and
+        //set the parent for certain number of items
+        //to find the leader, the index and the value should
+        //match
+        if(ele == parent[ele]) {
+            return ele;
+        }
+
+        int temp = find(parent[ele], parent);
+        parent[ele] = temp;
+        //this is the path compression optimization
         return temp;
     }
 
     public int findCircleNum(int[][] isConnected) {
-        //doesn't work on all test cases
+        //****************doesn't work on all test cases
         int[] parent = new int[isConnected.length];
         //initially all the values are just the index values itself
         //signifying that each one of the node is parent of itself
---
+++ IntelliJ/Graphs/src/Graph_Questions/DSU/Remove_max_No_of_edges.java
@@ -0,0 +1,11 @@
+package Graph_Questions.DSU;
+
+public class Remove_max_No_of_edges {
+    public static void main(String[] args) {
+        //https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/description/
+    }
+
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+
+    }
+}
